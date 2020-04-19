2d9a126a2d27d434a8e411a805b9e851c41c3030
Steve Boyd <emteknetnz@gmail.com>
2020-04-19 23:25:32 +1200

    Debug travis login page

---
+++ app/MyLoginContext.php
@@ -0,0 +1,17 @@
+<?php
+
+use SilverStripe\BehatExtension\Context\LoginContext;
+
+class MyLoginContext extends LoginContext
+{
+    public function stepILogInWith($email, $password)
+    {
+        $c = $this->getMainContext();
+        $loginUrl = $c->joinUrlParts($c->getBaseUrl(), $c->getLoginUrl());
+        $this->getMainContext()->getSession()->visit($loginUrl);
+        $page = $this->getMainContext()->getSession()->getPage();
+        assertNull('debug: ' . $page->getOuterHtml(), '... debug page outerhtml ...');
+
+        parent::stepILogInWith($email, $password);
+    }
+}
